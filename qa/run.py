c7c97ee4c714aabf4ed5fd5b967f11de756a7a47
Alvaro Lopez Ortega <alvaro@alobbs.com>
2013-09-07 09:50:56 +0200

    Implements --debug

--- qa/run.py
+++ qa/run.py
@@ -7,6 +7,8 @@ __copyright__ = "Copyright (C) 2013 Alvaro Lopez Ortega"
 import os
 import re
 import sys
+import logging
+import argparse
 import subprocess
 
 import util
@@ -24,10 +26,21 @@ def get_tests():
 
 
 def main():
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('--debug', action="store_true", help="Enable debugging")
+    ns = parser.parse_args()
+
+    if ns.debug:
+        logging.basicConfig (level = logging.DEBUG)
+
+    # Execute tests
     for f in get_tests():
         print (util.green(f))
         fp = os.path.join (qa_dir, f)
+        logging.info ('Executing: %s %s'%(sys.executable, fp))
         subprocess.check_call([sys.executable, fp])
+
 
 if __name__ == '__main__':
     main()
